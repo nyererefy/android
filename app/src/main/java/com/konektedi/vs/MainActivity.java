0b206244b822bfc618b77f0a37b915958cd96516
Sylvanus Kateile <kateile@outlook.com>
2018-06-01 10:58:42 +0300

    Done with support layout

--- app/src/main/java/com/konektedi/vs/MainActivity.java
+++ app/src/main/java/com/konektedi/vs/MainActivity.java
@@ -95,7 +95,6 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-
     private void showAbout() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
