17411a10dc58519bb2834ea1520612e7c739937d
Sylvanus Kateile <kateile@outlook.com>
2019-10-14 20:48:31 +0300

    remove useless files and dependencies

--- app/src/main/java/com/nyererefy/utilities/common/Constants.kt
+++ app/src/main/java/com/nyererefy/utilities/common/Constants.kt
@@ -3,18 +3,10 @@ package com.nyererefy.utilities.common
 object Constants {
     const val ID = "id"
     const val NAME = "name"
-    const val EMAIL = "email"
-    const val PASSWORD = "password"
     const val USERNAME = "username"
     const val NYEREREFY_PREFERENCES = "nyererefy_preferences"
-    const val DEVICE = "device"
-
-    const val POST = "post"
-    const val FILE = "file"
 
     //Paging Library
     const val INITIAL_LOAD = 5
     const val PAGE_SIZE = 10 //2 * Initial load
-
-    const val TOKEN = "token"
 }
